be21c778d9fe3e8b3c19966f310d7e7d1197228b
manchan13 <13manchan13@gmail.com>
2020-01-19 02:08:08 +0900

    TODO：DB接続が必要

--- complete/src/main/java/com/example/securingweb/entity/LoginUser.java
+++ complete/src/main/java/com/example/securingweb/entity/LoginUser.java
@@ -1,29 +1,29 @@
-package com.example.securingweb.entity;
-
-
-import lombok.Data;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
-/**
- * ログインユーザのユーザ名、パスワードを格納するためのEntity
- *
- */
-@Data
-@Entity
-@Table(name = "userTbl")
-public class LoginUser {
-
-    @Column(name = "id")
-    @Id
-    private Long id;
-
-    @Column(name = "name")
-    private String name;
-
-    @Column(name = "password")
-    private String password;
-
-}
+//package com.example.securingweb.entity;
+//
+//
+//import lombok.Data;
+//import javax.persistence.Column;
+//import javax.persistence.Entity;
+//import javax.persistence.Id;
+//import javax.persistence.Table;
+//
+///**
+// * ログインユーザのユーザ名、パスワードを格納するためのEntity
+// *
+// */
+//@Data
+//@Entity
+//@Table(name = "userTbl")
+//public class LoginUser {
+//
+//    @Column(name = "id")
+//    @Id
+//    private Long id;
+//
+//    @Column(name = "name")
+//    private String name;
+//
+//    @Column(name = "password")
+//    private String password;
+//
+//}
